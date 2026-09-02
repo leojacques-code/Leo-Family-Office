790eaf510497068668acaa2319ee7f54588b7b58
leojacques-code <leojacques.chety@gmail.com>
2026-09-02 16:28:34 +0200

    preview: route about page to V8

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,5 @@
-import { PublicV4Page } from "@/components/marketing/public-v4";
+import { PublicV8Page } from "@/components/marketing/public-v8";
 
 export default function AboutPage() {
-  return <PublicV4Page kind="about" />;
+  return <PublicV8Page kind="about" />;
 }
